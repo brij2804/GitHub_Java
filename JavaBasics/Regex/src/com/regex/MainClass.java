29d8beed003b7b800ba38b5ec8d10022e242e2f8
brij2804 <brij2804@users.noreply.github.com>
2018-12-05 12:01:39 +0530

    added pattern matchers

--- JavaBasics/Regex/src/com/regex/MainClass.java
+++ JavaBasics/Regex/src/com/regex/MainClass.java
@@ -7,6 +7,9 @@ public class MainClass {
     public static void main(String[] args){
         RegexMatcher regexMatcher= new RegexMatcher();
 
-        regexMatcher.method1();
+        //regexMatcher.method1();
+        //regexMatcher.method2();
+        //regexMatcher.method3();
+         regexMatcher.method4();
     }
 }
--- JavaBasics/Regex/src/com/regex/inputs/InputStrings.java
+++ JavaBasics/Regex/src/com/regex/inputs/InputStrings.java
@@ -7,4 +7,8 @@ public class InputStrings {
     public static String INPUT_STR_4= "peach at 10p";
     public static String INPUT_STR_5= "20 sales of peaches at 10p each";
     public static String INPUT_STR_6= "Add 20p peaches";
+    public static String INPUT_NUMBER = "123456788";
+    public static String INPUT_NUMBER_1 = "123456788abc";
+    public static String INPUT_NUMBER_LENGTH_6="123456";
+    public static String INPUT_NUMBER_LENGHT_10="1234567890";
 }
