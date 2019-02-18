f2d54252dab06293627064c6351e0ce7b7a1cf75
brij2804 <brij2804@users.noreply.github.com>
2019-02-18 22:12:27 +0530

    adding spring jpa project file

---
+++ Hibernate/SpringJPAMysql/src/main/java/com/brijesh/common/CollegeConstants.java
@@ -0,0 +1,7 @@
+package com.brijesh.common;
+
+public class CollegeConstants {
+
+    public static final String STUDENT_SERVICE = "/college/v1/student/";
+    public static final String ERROR_PROCESSING_STUDENT_SERVICE_REQUEST = "Exception occurred while processing student service request.";
+}
