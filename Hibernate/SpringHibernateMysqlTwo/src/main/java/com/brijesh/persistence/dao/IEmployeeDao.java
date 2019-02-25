bc23215dc5ef2e340da6113b0f3a629a9be6a11f
brij2804 <brij2804@users.noreply.github.com>
2019-02-25 17:16:20 +0530

    adding files to project

---
+++ Hibernate/SpringHibernateMysqlTwo/src/main/java/com/brijesh/persistence/dao/IEmployeeDao.java
@@ -0,0 +1,8 @@
+package com.brijesh.persistence.dao;
+
+import com.brijesh.persistence.dao.common.IOperations;
+import com.brijesh.persistence.model.Employee;
+
+public interface IEmployeeDao extends IOperations<Employee> {
+
+}
