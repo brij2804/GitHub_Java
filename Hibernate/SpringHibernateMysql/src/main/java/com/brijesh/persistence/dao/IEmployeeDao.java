95fbe0a7820592b48685e591142845c7b933cacb
brij2804 <brij2804@users.noreply.github.com>
2019-02-25 13:40:55 +0530

    adding file for hibernate project

--- Hibernate/SpringHibernateMysql/src/main/java/com/brijesh/persistence/dao/IEmployeeDao.java
+++ Hibernate/SpringHibernateMysql/src/main/java/com/brijesh/persistence/dao/IEmployeeDao.java
@@ -1,4 +1,8 @@
 package com.brijesh.persistence.dao;
 
-public interface IEmployeeDao {
+import com.brijesh.persistence.dao.common.IOperations;
+import com.brijesh.persistence.model.Employee;
+
+public interface IEmployeeDao extends IOperations<Employee> {
+
 }
--- Hibernate/SpringHibernateMysql/src/main/java/com/brijesh/persistence/dataservice/EmployeeDataService.java
+++
@@ -1,7 +0,0 @@
-package com.brijesh.persistence.dataservice;
-
-public interface EmployeeDataService {
-
-    // void save();
-    // List<User> list();
-}
