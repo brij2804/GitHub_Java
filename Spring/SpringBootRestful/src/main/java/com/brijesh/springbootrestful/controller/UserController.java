9dca1cd0a10796ea140bd18e42ac1a2d0d5671bf
brij2804 <brij2804@users.noreply.github.com>
2019-11-04 21:40:21 +0530

    new code for restful service

--- Spring/SpringBootRestful/src/main/java/com/brijesh/springbootrestful/controller/UserController.java
+++ Spring/SpringBootRestful/src/main/java/com/brijesh/springbootrestful/controller/UserController.java
@@ -4,10 +4,12 @@ import com.brijesh.springbootrestful.beans.User;
 import com.brijesh.springbootrestful.exception.UserNotFoundException;
 import com.brijesh.springbootrestful.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -40,9 +42,20 @@ public class UserController {
     // output - created and return the created uri
     //http://localhost:8080/users
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
         User savedUser= service.save(user);
         URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
+
+
+    //GET /users/{id}
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user==null){
+            throw new UserNotFoundException("id-"+id);
+        }
+    }
+
 }
