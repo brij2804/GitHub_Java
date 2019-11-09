c13248d2376713b2445a5f2913b8a7b14ccfb2dd
brij2804 <brij2804@users.noreply.github.com>
2019-11-09 21:12:24 +0530

    adding new currency conversion service

---
+++ Microservices/SpringbootMicroservicesPart2/currency-conversion-service/src/main/java/com/brijesh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,17 @@
+package com.brijesh.microservices.currencyconversionservice.controller;
+
+import com.brijesh.microservices.currencyconversionservice.beans.CurrencyConversionBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+     return new CurrencyConversionBean(1L,from,to,BigDecimal.ONE,quantity,quantity,0);
+    }
+}
