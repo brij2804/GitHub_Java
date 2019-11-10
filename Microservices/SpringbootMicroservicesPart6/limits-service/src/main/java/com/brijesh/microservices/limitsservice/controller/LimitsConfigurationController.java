179a85d356aec7420486b2a853248c93ee29b233
brij2804 <brij2804@users.noreply.github.com>
2019-11-11 00:50:04 +0530

    Netflix Hystrix fallback

--- Microservices/SpringbootMicroservicesPart6/limits-service/src/main/java/com/brijesh/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ Microservices/SpringbootMicroservicesPart6/limits-service/src/main/java/com/brijesh/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.brijesh.microservices.limitsservice.controller;
 
 import com.brijesh.microservices.limitsservice.beans.LimitConfiguration;
 import com.brijesh.microservices.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-limits")
+    @HystrixCommand(fallbackMethod = "fallbackretrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackretrieveConfiguration() {
+        return new LimitConfiguration(9, 9999);
     }
 }
