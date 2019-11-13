a67ad892da5b5cfcf0a054f9324ec957f76c17b1
brij2804 <brij2804@users.noreply.github.com>
2019-11-13 14:42:51 +0530

    changed the folder structure

--- Spring/SpringBootRestfulPart3/springboot-restful-part3/src/main/java/com/brijesh/springbootrestful/controller/UserJPAController.java
+++ Spring/SpringBootRestfulPart3/springboot-restful-part3/src/main/java/com/brijesh/springbootrestful/controller/UserJPAController.java
@@ -26,53 +26,53 @@ public class UserJPAController {
     private PostRepository postRepository;
 
     @GetMapping("/jpa/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/jpa/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public User retrieveUser(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
 
-        if(!user.isPresent()){
-          throw new UserNotFoundException("id-"+id);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
         }
         return user.get();
     }
 
     @PostMapping("/jpa/users")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
-        User savedUser= userRepository.save(user);
-        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
 
     @DeleteMapping("/jpa/users/{id}")
-    public void deleteUser(@PathVariable int id){
+    public void deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
     }
 
-       @GetMapping("/jpa/users/{id}/posts")
-    public List<Post> retrieveAllUsers(@PathVariable int id ){
-        Optional<User> userOptional=  userRepository.findById(id);
-        if(!userOptional.isPresent()){
-            throw new UserNotFoundException("id -"+id);
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> retrieveAllUsers(@PathVariable int id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("id -" + id);
         }
         return userOptional.get().getPosts();
     }
 
     @PostMapping("/jpa/users/{id}/posts")
-    public ResponseEntity<Object> createPost(@PathVariable int id,@RequestBody Post post ){
-        Optional<User> userOptional=  userRepository.findById(id);
-        if(!userOptional.isPresent()){
-            throw new UserNotFoundException("id -"+id);
+    public ResponseEntity<Object> createPost(@PathVariable int id, @RequestBody Post post) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("id -" + id);
         }
 
-        User user=userOptional.get();
+        User user = userOptional.get();
         post.setUser(user);
         postRepository.save(post);
 
-        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
 
