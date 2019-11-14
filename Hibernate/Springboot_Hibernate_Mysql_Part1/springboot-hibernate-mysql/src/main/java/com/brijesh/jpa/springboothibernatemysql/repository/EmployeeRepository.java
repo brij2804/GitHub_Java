ddb79153cbccc7c9a5d54d0574e30a08770371e8
brij2804 <brij2804@users.noreply.github.com>
2019-11-14 22:45:44 +0530

    springboot hibernate mysql basic code sample

--- Hibernate/Springboot_Hibernate_Mysql_Part1/springboot-hibernate-mysql/src/main/java/com/brijesh/jpa/springboothibernatemysql/repository/EmployeeRepository.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part1/springboot-hibernate-mysql/src/main/java/com/brijesh/jpa/springboothibernatemysql/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.brijesh.jpa.springboothibernatemysql.repository;
 
 import com.brijesh.jpa.springboothibernatemysql.entity.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
     List<Employee> findByName(String name);
