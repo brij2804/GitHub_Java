c0c398154d7742ab71395626ca301bf35bbca461
brij2804 <brij2804@users.noreply.github.com>
2019-11-18 18:22:01 +0530

    springboot jdbc H2 database basic code example

--- Hibernate/Springboot_Hibernate_Mysql_Part2/springboot-jdbc-h2/src/main/java/com/brijesh/jdbc/springbootjdbch2/jdbc/PersonJdbcDao.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part2/springboot-jdbc-h2/src/main/java/com/brijesh/jdbc/springbootjdbch2/jdbc/PersonJdbcDao.java
@@ -27,8 +27,8 @@ public class PersonJdbcDao {
         return lst;
     }
 
-    public Person deleteById(int id) {
-        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[]{id}, new BeanPropertyRowMapper<Person>(Person.class));
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", new Object[]{id}, new BeanPropertyRowMapper<Person>(Person.class));
     }
 
 }
