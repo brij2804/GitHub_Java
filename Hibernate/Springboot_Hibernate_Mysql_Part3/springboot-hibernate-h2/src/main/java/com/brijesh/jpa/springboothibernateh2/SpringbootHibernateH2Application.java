a868030f64e8ca8f812e115ff923efccd44ae8ca
brij2804 <brij2804@users.noreply.github.com>
2019-11-21 22:59:42 +0530

    springboot jpa hibernate advanced code

---
+++ Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/SpringbootHibernateH2Application.java
@@ -0,0 +1,13 @@
+package com.brijesh.jpa.springboothibernateh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHibernateH2Application.class, args);
+    }
+
+}
