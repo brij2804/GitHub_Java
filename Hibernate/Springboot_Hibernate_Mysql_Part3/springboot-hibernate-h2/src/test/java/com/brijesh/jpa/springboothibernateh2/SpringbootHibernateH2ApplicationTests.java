331ad9339878912690f0ab0ca037891fc622e821
brij2804 <brij2804@users.noreply.github.com>
2019-11-22 11:39:10 +0530

    springboot jpa hibernate advanced code

--- Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/test/java/com/brijesh/jpa/springboothibernateh2/SpringbootHibernateH2ApplicationTests.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/test/java/com/brijesh/jpa/springboothibernateh2/SpringbootHibernateH2ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringbootHibernateH2ApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
---
+++ Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/test/java/com/brijesh/jpa/springboothibernateh2/repository/CourseRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.brijesh.jpa.springboothibernateh2.repository;
+
+import com.brijesh.jpa.springboothibernateh2.SpringbootHibernateH2Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringbootHibernateH2Application.class)
+public class CourseRepositoryTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
