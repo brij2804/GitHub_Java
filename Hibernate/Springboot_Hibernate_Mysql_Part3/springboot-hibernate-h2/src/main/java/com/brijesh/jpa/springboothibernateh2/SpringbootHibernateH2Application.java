5bd993f7c112ffbfdc0557cfda97d8da58ba05f4
brij2804 <brij2804@users.noreply.github.com>
2019-11-22 17:13:04 +0530

    springboot jpa hibernate advanced code

--- Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/SpringbootHibernateH2Application.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/SpringbootHibernateH2Application.java
@@ -26,7 +26,7 @@ public class SpringbootHibernateH2Application implements CommandLineRunner {
         Course course = courseRepository.findById(10001L);
         logger.info("Find by Id 10001-> {} ", course);
 
-        courseRepository.deleteById(10001L);
+        courseRepository.deleteById(10001l);
 
     }
 }
--- Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/repository/CourseRepository.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part3/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/repository/CourseRepository.java
@@ -3,14 +3,19 @@ package com.brijesh.jpa.springboothibernateh2.repository;
 import com.brijesh.jpa.springboothibernateh2.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 
 @Repository
+@Transactional
 public class CourseRepository {
 
     @Autowired
-    EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public Course findById(Long id) {
         return entityManager.find(Course.class, id);
