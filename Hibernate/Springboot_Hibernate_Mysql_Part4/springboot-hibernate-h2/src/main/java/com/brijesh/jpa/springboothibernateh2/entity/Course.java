0425ef09559cb0cefebecbe7ca76762655a86a3c
brij2804 <brij2804@users.noreply.github.com>
2019-11-28 10:48:58 +0530

    springboot jpa hibernate advanced code

--- Hibernate/Springboot_Hibernate_Mysql_Part4/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/entity/Course.java
+++ Hibernate/Springboot_Hibernate_Mysql_Part4/springboot-hibernate-h2/src/main/java/com/brijesh/jpa/springboothibernateh2/entity/Course.java
@@ -1,9 +1,6 @@
 package com.brijesh.jpa.springboothibernateh2.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "course")
@@ -13,6 +10,7 @@ public class Course {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "fullname", nullable = false)
     private String name;
 
     public Course(String name) {
