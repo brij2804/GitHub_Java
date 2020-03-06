752a58f7da77108f2c758a6cf69a3459983d0803
brij2804 <brij2804@users.noreply.github.com>
2020-03-06 10:14:26 +0530

    springboot jpa hibernate advanced code. Adding testcases and features of hibernate annotation
    Adding timestamp code

--- Hibernate/Springboot_Hibernate_Mysql_Part4/springboot-hibernate-h2/src/main/resources/data.sql
+++ Hibernate/Springboot_Hibernate_Mysql_Part4/springboot-hibernate-h2/src/main/resources/data.sql
@@ -1,7 +1,8 @@
-insert into course(id,fullname) values(10001,'Brijesh');
-insert into course(id,fullname) values(10002,'Rachel');
-insert into course(id,fullname) values(10003,'Phoebe');
-insert into course(id,fullname) values(10004,'Joey');
-insert into course(id,fullname) values(10005,'Monica');
-insert into course(id,fullname) values(10006,'Chandler');
-insert into course(id,fullname) values(10007,'Ross');
+insert into course(id,fullname,created_date,last_updated_date)
+values(10001,'Brijesh',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10002,'Rachel',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10003,'Phoebe',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10004,'Joey',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10005,'Monica',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10006,'Chandler',sysdate(),sysdate());
+insert into course(id,fullname,created_date,last_updated_date) values(10007,'Ross',sysdate(),sysdate());
